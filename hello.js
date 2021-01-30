69de7934b439e5eabeac14c2928d39ea70f6d0ab
Tahsin Alam Tanim <tanimtahsi124@gmail.comm>
2021-01-30 18:32:33 +0600

    tanim

---
+++ hello.js
@@ -0,0 +1,25 @@
+document.getElementById("first-btn").addEventListener("click" , function(){
+    var loveMgs= document.getElementById("love-mgs").innerText;
+   document.getElementById("love-mgs").style.display = "block";
+
+    function tanim (){
+        for(var i =1; i<20 ; i++)
+      var  tanim = document.write(loveMgs +"</br>");
+    }
+     tanim() 
+     document.write("</br>" + "কি ভেবেছো প্রোপোজ করতেই থাকবো? ভালোবাসা কি অলিগলিতে বাস করে নাকি হ্যাঁ??????")  
+   })
+
+   document.getElementById("second-btn").addEventListener("click" , function(){
+    var loveMgs= document.getElementById("love-mgs").innerText;
+   document.getElementById("love-mgs").style.display = "block";
+   function tahsin (){
+        for(var i =1; i<1000 ; i++)
+      var  tanim = document.write(loveMgs +"</br>" );
+
+    var tanu = document.write("</br>" + "এই মন তোমাকে দিলাম" + "</br>" + "</br>") ;
+
+    }
+     tahsin() 
+   
+   })
